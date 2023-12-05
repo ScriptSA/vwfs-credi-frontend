a72a0de364e7c5ca3ab158a363294e5e25d77fff
estbellan <33107491+estbellan@users.noreply.github.com>
2023-12-05 17:02:58 -0300

    Revert "ajustes iniciales, login, temas"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'login', loadChildren: () => import('../app/login/login.module').then(m => m.LoginModule)},
-  { path: 'main-frame', loadChildren: () => import('../app/main-frame/main-frame.module').then(m => m.MainFrameModule)},
-
-
-  // Add more routes for different roles as necessary
-  { path: '**', redirectTo: '/login', pathMatch: 'full' } // Redirect unmatched routes to login
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
