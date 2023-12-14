53da6483bb282bbbfd83834eb1dd4f37d2bb8a2b
nebella <estebanbellantonio@gmail.com>
2023-12-13 23:27:33 -0300

    ajustes admision inicial

--- src/app/admision-search/admision-search.component.ts
+++ src/app/admision-search/admision-search.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
+import { MatTabGroup } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-admision-search',
   templateUrl: './admision-search.component.html',
   styleUrls: []
 })
-export class AdmisionSearchComponent {
+export class AdmisionSearchComponent implements OnInit {
+  //controles del resultado de la búsqueda
+  searchForm: FormGroup;
+  searchResults = false;
+
+  //control de tabs
+  @ViewChild(MatTabGroup) tabGroup!: MatTabGroup;
+
+  //mock titulo tramites (hay que hacer el count)
+  totalPendingNumber = 20;
+  resultNumber = 2;
+
+  //search mock
+  constructor(
+    private formBuilder: FormBuilder)
+    {
+    this.searchForm = this.formBuilder.group({
+      search: ['']
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.searchResults=true;
+    this.tabGroup.selectedIndex = 1;
+  }
+
+  removeResults(){
+    this.searchResults=false;
+  }
 
 }
--- src/app/admision-search/admision-search.module.ts
+++ src/app/admision-search/admision-search.module.ts
@@ -2,11 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdmisionSearchComponent } from './admision-search.component';
 import { AdmisionSearchRoutingModule } from './admision-search-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   imports: [
     CommonModule,
-    AdmisionSearchRoutingModule
+    AdmisionSearchRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule
   ],
   declarations: [AdmisionSearchComponent],
 
