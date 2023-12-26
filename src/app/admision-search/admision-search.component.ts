d6cda32c8c7d36861d218e531baeb182475efd2b
estbellan <estebanbellantonio@gmail.com>
2023-12-26 15:46:45 -0300

    implemento tabla material angular

--- src/app/admision-search/admision-search.component.ts
+++ src/app/admision-search/admision-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatTabGroup } from '@angular/material/tabs';
+import { Admision } from '../shared/models/admision';
 
 @Component({
   selector: 'app-admision-search',
@@ -8,98 +9,26 @@ import { MatTabGroup } from '@angular/material/tabs';
   styleUrls: []
 })
 export class AdmisionSearchComponent implements OnInit {
-[x: string]: any;
+  [x: string]: any;
   //controles del resultado de la búsqueda
   searchForm: FormGroup;
   searchResults = false;
+  columnasBusqueda = ['estado', 'numero', 'carga', 'modificado', 'cliente', 'campania', 'concesionario', 'monto'];
+  tramites: Admision[] = [];
+  selectedTramite?: Admision;
+  loading = false;
+
+  detalleTramite(row: Admision) {
+    console.log(row);
+  }
 
   //mock tablas
   spoolerTable = {
     spools: [
-      {filterBy: ['sin Imágenes', 'en análisis']}
-    ],
-
-    heads: [
-      { name: ['nro de trámite', 'fecha y hora carga', 'última modificación', 'cliente', 'campaña', 'concesionario', 'monto'] }
-    ],
-
-    rows: [
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      { model: ['12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000 '] },
-      // Agrega más dos según sea necesario
+      { filterBy: ['sin Imágenes', 'en análisis'] }
     ]
   }
 
-  searchTable = {
-    heads: [
-      { name: ['estado', 'nro de trámite', 'fecha y hora carga', 'última modificación', 'cliente', 'campaña', 'concesionario', 'monto'] },
-    ],
-    rows: [
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      { model: ['en análisis', '12345', '25/10/2023 12:35', '25/10/2023 12:35', 'apellido largo, muchos nombres', 'nombre de campaña', 'nombre de concesionario', '1.000.000'] },
-      // Agrega más dos según sea necesario
-    ]
-  }
 
   //control de tabs
   @ViewChild(MatTabGroup) tabGroup!: MatTabGroup;
